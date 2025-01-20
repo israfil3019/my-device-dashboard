2bf62548d9dc369d34aaf83f378ffcc963fd1cfd
İsrafil Topaç <israfil.topac@one37id.com>
2025-01-20 13:35:23 +0100

    create route protection

---
+++ src/app/dashboard/components/ProtectedRoute.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+
+interface ProtectedRouteProps {
+  children: React.ReactNode;
+}
+
+const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      router.push('/login');
+    }
+  }, [router]);
+
+  return <>{children}</>;
+};
+
+export default ProtectedRoute;
