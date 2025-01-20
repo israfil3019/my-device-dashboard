7cff9d8c4f0d809f0c173ed30ebfeafc68c363f2
İsrafil Topaç <israfil.topac@one37id.com>
2025-01-20 14:00:05 +0100

    refactor: use tabscontext inside tab component

--- src/app/dashboard/components/Tabs.tsx
+++ src/app/dashboard/components/Tabs.tsx
@@ -1,37 +1,36 @@
-import React, { useState } from 'react';
+"use client";
+
+import React from "react";
+import { useTabsContext } from "@/context/TabsContext";
 
 interface Tab {
   label: string;
   content: React.ReactNode;
 }
-
 interface TabsProps {
   tabs: Tab[];
-  defaultActiveTab?: number; // Optional, to set the default active tab
 }
 
-export default function Tabs({ tabs, defaultActiveTab = 0 }: TabsProps) {
-  const [activeTab, setActiveTab] = useState(defaultActiveTab);
+export default function Tabs({ tabs }: TabsProps) {
+  const { activeTab, setActiveTab } = useTabsContext();
 
   return (
     <div>
-      {/* Tab Headers */}
-      <div className="flex gap-4 mb-6">
+      <div className="flex justify-center gap-4 mb-6 px-4">
         {tabs.map((tab, index) => (
           <button
             key={index}
-            className={`px-4 py-2 rounded ${
-              activeTab === index ? 'bg-blue-500 text-white' : 'bg-gray-200'
+            className={`px-4 py-2 rounded w-full text-2xl ${
+              activeTab === tab.label ? "bg-blue-500 text-white" : "bg-gray-200"
             }`}
-            onClick={() => setActiveTab(index)}
+            onClick={() => setActiveTab(tab.label)}
           >
             {tab.label}
           </button>
         ))}
       </div>
 
-      {/* Active Tab Content */}
-      <div>{tabs[activeTab]?.content}</div>
+      <div>{tabs.find((tab) => tab.label === activeTab)?.content}</div>
     </div>
   );
 }
