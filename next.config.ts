374612c3662f90b5207365d04e491e0b0c7b1844
İsrafil Topaç <israfil.topac@one37id.com>
2025-01-22 12:20:12 +0100

    update mock data

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = {
   async rewrites() {
     return [
       {
-        source: '/api/:path*',
-        destination: 'https://staging-api.arbolitics.com/:path*', // Proxy to API
+        source: "/api/:path*",
+        destination: "https://staging-api.arbolitics.com/:path*",
       },
     ];
   },
