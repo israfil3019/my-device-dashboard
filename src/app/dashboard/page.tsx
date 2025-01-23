53a4769e58d3f686d2a17e539f71d94d6109b0c4
İsrafil Topaç <israfil.topac@one37id.com>
2025-01-23 02:04:06 +0100

    remove queryclient from dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,39 +1,34 @@
-'use client';
+"use client";
 
-import { QueryClient, QueryClientProvider } from 'react-query';
-import Tabs from './components/Tabs';
-import DataTable from './components/DataTable';
-import ChartPage from './components/ChartPage'; 
-import Navbar from './components/Navbar';
-import ProtectedRoute from './components/ProtectedRoute';
-import { TabsProvider } from '@/context/TabsContext';
-
-const queryClient = new QueryClient();
+import Tabs from "./components/Tabs";
+import DataTable from "./components/DataTable";
+import ChartPage from "./components/ChartPage";
+import Navbar from "./components/Navbar";
+import ProtectedRoute from "./components/ProtectedRoute";
+import { TabsProvider } from "@/context/TabsContext";
 
 export default function DashboardPage() {
   const tabs = [
     {
-      label: 'Charts',
+      label: "Charts",
       content: <ChartPage />,
     },
     {
-      label: 'AG Grid',
+      label: "AG Grid",
       content: <DataTable />,
     },
   ];
 
   return (
     <ProtectedRoute>
-      <QueryClientProvider client={queryClient}>
-        <TabsProvider>
-          <Navbar />
-          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-            <div className="min-h-screen bg-gray-50 pt-20">
-              <Tabs tabs={tabs} />
-            </div>
+      <TabsProvider>
+        <Navbar />
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="min-h-screen bg-gray-50 pt-20">
+            <Tabs tabs={tabs} />
           </div>
-        </TabsProvider>
-      </QueryClientProvider>
+        </div>
+      </TabsProvider>
     </ProtectedRoute>
   );
 }
