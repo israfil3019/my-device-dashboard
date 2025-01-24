612dfc9dd75f7220b16f7268f9cbcd86d8beb6a9
İsrafil Topaç <israfil.topac@one37id.com>
2025-01-24 15:41:37 +0100

    check if user authenticated and direct

--- src/app/dashboard/components/ProtectedRoute.tsx
+++ src/app/dashboard/components/ProtectedRoute.tsx
@@ -1,7 +1,8 @@
-'use client';
+"use client";
 
-import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
+import Spinner from "@/app/loading/Spinner";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 interface ProtectedRouteProps {
   children: React.ReactNode;
@@ -9,15 +10,42 @@ interface ProtectedRouteProps {
 
 const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
   const router = useRouter();
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null);
 
   useEffect(() => {
-    const token = localStorage.getItem('token');
-    if (!token) {
-      router.push('/login');
-    }
+    const checkAuthentication = async () => {
+      try {
+        const response = await fetch("/api/proxy/me", {
+          method: "GET",
+          credentials: "include", // Include cookies in the request
+        });
+
+        if (response.ok) {
+          const data = await response.json();
+          console.log("Authenticated user data:", data);
+
+          // User is authenticated, allow access to the dashboard
+          setIsAuthenticated(true);
+        } else {
+          // Token is invalid or missing, redirect to login
+          setIsAuthenticated(false);
+          router.push("/login");
+        }
+      } catch (error) {
+        console.error("Error checking authentication:", error);
+        setIsAuthenticated(false);
+        router.push("/login");
+      }
+    };
+
+    checkAuthentication();
   }, [router]);
 
-  return <>{children}</>;
+  if (isAuthenticated === null) {
+    return <Spinner overlay={true} />;
+  }
+
+  return <>{isAuthenticated ? children : null}</>;
 };
 
 export default ProtectedRoute;
